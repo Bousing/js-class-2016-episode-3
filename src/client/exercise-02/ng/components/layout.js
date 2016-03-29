7a035e7820921dc3cde5c9e3e61c5b7afaf84bb4
khalef_a <khalef_a@epita.fr>
2016-03-29 12:53:16 +0200

    Exo 1 et 2 done

--- src/client/exercise-02/ng/components/layout.js
+++ src/client/exercise-02/ng/components/layout.js
@@ -1,4 +1,5 @@
 import 'client/exercise-02/ng/services/chuck-norris';
+import 'client/exercise-02/ng/components/fact';
 
 window._app.global_ng_module
 .component('layout', {
@@ -6,6 +7,7 @@ window._app.global_ng_module
   controller: ['chuckNorris', function(chuckNorris) {
     chuckNorris.fetch3Random().then((res) => {
       console.log(res.data.value);
+      this.facts = res.data.value;
     })
   }]
 });
